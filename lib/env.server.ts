3fd53b418045d9307362f80402fc35327f14fd38
SamW221 <7mriusa@gmail.com>
2026-06-13 10:23:46 -0700

    Use Vercel URL for auth redirects

--- lib/env.server.ts
+++ lib/env.server.ts
@@ -14,5 +14,17 @@ export function getSupabaseServiceRoleEnv() {
 }
 
 export function getAppUrl() {
-  return process.env.APP_URL || "http://localhost:3000";
+  if (process.env.APP_URL) {
+    return process.env.APP_URL;
+  }
+
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return "http://localhost:3000";
 }
